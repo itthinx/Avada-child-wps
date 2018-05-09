521ba49897183eeb52eb32331d5c2e022538fe52
itthinx <itthinx@gmail.com>
2018-05-09 15:42:02 +0200

    added ABSPATH check, header and revised comments

--- functions.php
+++ functions.php
@@ -1,9 +1,27 @@
 <?php
+/**
+ * Integrates Avada search forms with WooCommerce Product Search to use the live product search field instead.
+ * Loads the stylesheet with necessary overrides.
+ *
+ * @author     itthinx
+ * @link       https://www.itthinx.com
+ * @package    Avada-child-wps
+ * @since      1.0.0
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Returns the live product search form if WooCommerce Product Search is active. Otherwise it will return an empty string.
+ *
+ * Uses the standard search form replacement if it is enabled. This will show the form based on the settings
+ * under WooCommerce > Settings > Search > General : Standard Product Search : Product Search Field settings ...
+ * If the standard search form replacement is not enabled, we will render the live product search field using
+ * its default settings.
+ */
 function avada_child_wps_search_form() {
-	// Use the standard search form replacement if it is enabled. This will show the form based on the settings
-	// under WooCommerce > Settings > Search > General : Standard Product Search : Product Search Field settings ...
-	// If the standard search form replacement is not enabled, we will render the live product search field using
-	// its default settings.
 	$form = '';
 	if ( class_exists( 'WooCommerce_Product_Search_Field' ) && method_exists( 'WooCommerce_Product_Search_Field', 'get_product_search_form' ) ) {
 		$form = WooCommerce_Product_Search_Field::get_product_search_form( '' );
